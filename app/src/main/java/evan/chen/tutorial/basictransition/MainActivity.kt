023c05c6207e9e0ea126910751de7d112c06b32b
Evan <pifan76@gmail.com>
2018-10-29 06:39:16 +0800

    Initial commit

---
+++ app/src/main/java/evan/chen/tutorial/basictransition/MainActivity.kt
@@ -0,0 +1,37 @@
+package evan.chen.tutorial.basictransition
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.transition.*
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val scene1 = Scene(scene_root, scene_root.findViewById(R.id.container) as ViewGroup)
+
+        val scene2 = Scene.getSceneForLayout(scene_root, R.layout.scene2, this)
+
+        val scene3 = Scene.getSceneForLayout(scene_root, R.layout.scene3, this)
+
+        scene1Button.setOnClickListener {
+            // 到Scene1
+            TransitionManager.go(scene1)
+        }
+
+        scene2Button.setOnClickListener {
+            // 到Scene2
+            TransitionManager.go(scene2)
+        }
+
+        scene3Button.setOnClickListener {
+            // 到Scene3
+            TransitionManager.go(scene3)
+        }
+    }
+}
+
